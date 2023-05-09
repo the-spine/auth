75f3f09a7cf9be3d3ed1eb38911ec01c4ecad24c
GT020 <gauravtanwar0020@gmail.com>
2023-05-09 20:46:44 +0530

    Add Basic Config Setup

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+import (
+	"auth/internal/config"
+	"fmt"
+)
+
+var konfig *config.Config
+
+func init() {
+	var err error
+	konfig, err = config.LoadConfig("./")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	fmt.Println(*konfig)
+}
