595a60e8372b179fd9f7db87c16d998d9fe3b9b4
GT020 <gauravtanwar0020@gmail.com>
2023-05-17 20:22:29 +0530

    Update User Model

--- internal/models/user.go
+++ internal/models/user.go
@@ -7,14 +7,14 @@ import (
 )
 
 type User struct {
-	TenantID     uuid.UUID `gorm:"type:uuid;not null"`
-	ID           uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-	FirstName    string    `gorm:"size:50;not null"`
-	MiddleName   string    `gorm:"size:50"`
-	LastName     string    `gorm:"size:50;not null"`
-	Email        string    `gorm:"size:100;uniqueIndex;not null"`
-	PasswordHash string    `gorm:"size:100;not null"`
-	Roles        []Role    `gorm:"many2many:user_roles"`
-	CreatedAt    time.Time `gorm:"not null"`
-	UpdatedAt    time.Time `gorm:"not null"`
+	TenantID     *uuid.UUID `gorm:"type:uuid"`
+	ID           *uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
+	FirstName    string     `gorm:"size:50;not null"`
+	MiddleName   string     `gorm:"size:50"`
+	LastName     string     `gorm:"size:50;not null"`
+	Email        string     `gorm:"size:100;uniqueIndex;not null"`
+	PasswordHash string     `gorm:"size:100;not null"`
+	Roles        []Role     `gorm:"many2many:user_roles"`
+	CreatedAt    time.Time  `gorm:"not null;default:now()"`
+	UpdatedAt    time.Time  `gorm:"not null;default:now()"`
 }
